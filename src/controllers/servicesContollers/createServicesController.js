ec3493de315105805e79e569c4fa08cc85d9dfc2
F.Fernandez <hellsingsd@gmail.com>
2024-07-12 01:40:05 -0300

    falta update services nomas en servicios

--- src/controllers/servicesContollers/createServicesController.js
+++ src/controllers/servicesContollers/createServicesController.js
@@ -3,6 +3,8 @@ const User = require("../../DB/models/User");
 
 const createServicesController = async (service, category, price, sing, type) => {
   try {
+    console.log('entrando al controlador')
+
     const lowerCaseService = service.toLowerCase();
     const lowerCaseCategory = category.toLowerCase();
 
@@ -12,6 +14,7 @@ const createServicesController = async (service, category, price, sing, type) =>
     let isServiceAdded = false;
 
     if (!existingServiceDoc) {
+      console.log('entra si no existe el documento')
       // Si no existe, crear un nuevo documento con la nueva categoría y servicio
       existingServiceDoc = new Services({
         services: {
@@ -29,44 +32,71 @@ const createServicesController = async (service, category, price, sing, type) =>
       await existingServiceDoc.save();
       isServiceAdded = true;
     } else {
+      console.log('entra si existe un documento services')
       // Entrará al if solo si los servicios en DB son distintos a un objeto vacío
-      if (Object.keys(existingServiceDoc.services).length > 0) {
-        for (let category in existingServiceDoc.services) {
-          if (existingServiceDoc.services[category][lowerCaseService]) {
-            throw new Error("El servicio ya existe"); // Si el servicio existe en alguna categoría retorna
-          }
-        }
-
-        // Si no se encontró el servicio en ninguna categoría, procede a agregarlo
-        if (existingServiceDoc.services[lowerCaseCategory]) {  // Existe la categoría
-          // Agregar el nuevo servicio a la categoría existente sin sobrescribir los existentes
-          existingServiceDoc.services[lowerCaseCategory][lowerCaseService] = {
+      if (Object.keys(existingServiceDoc.services).length < 1) {
+        console.log('deberia entrar la propiedad es menor q 1')
+        existingServiceDoc.services[lowerCaseCategory] = {
+          [lowerCaseService]: {
             price,
             sing,
-            type
-          };
-          isServiceAdded = true;
-        } else {
-          // Crear la nueva categoría con el nuevo servicio
-          existingServiceDoc.services[lowerCaseCategory] = {
-            [lowerCaseService]: {
-              price,
-              sing,
-              type
-            }
-          };
-          isServiceAdded = true;
+            type,
+          }
         }
         existingServiceDoc.markModified("services");
-        await existingServiceDoc.save();
+        await existingServiceDoc.save()
+        isServiceAdded = true
+      } else { // no es un objeto vacio
+        console.log('entra si no es un objeto vacio')
+        if (existingServiceDoc.services[lowerCaseCategory]) {
+          console.log('entra si existe el servicio')
+          if (existingServiceDoc.services[lowerCaseCategory][lowerCaseService]) {
+            console.log('entra si el servicio ya existe en la categoria')
+            throw new Error("El servicio ya existe en esta categoria")
+          } else {
+            console.log('entra si la categoria no existe')
+            for (let category in existingServiceDoc.services) {
+              if (existingServiceDoc.services[category][lowerCaseService]) {
+                console.log('entra si existe en otra categoria')
+                throw new Error("El servicio ya existe en otra categoria"); // Si el servicio existe en alguna categoría retorna
+              }
+              console.log('entra si no existe en otra categoria y lo crea')
+              existingServiceDoc.services[lowerCaseCategory][lowerCaseService] = {
+                price,
+                sing,
+                type,
+              };
+              isServiceAdded = true
+            }
+          }
+
+
+        } else {
+          console.log('entra si la categoria no existe')
+          for (let category in existingServiceDoc.services) {
+            if (existingServiceDoc.services[category][lowerCaseService]) {
+              console.log('entra si existe en otra categoria')
+              throw new Error("El servicio ya existe en otra categoria"); // Si el servicio existe en alguna categoría retorna
+            }
+            console.log('entra si lo crea cuando no existe en otra categoria')
+            existingServiceDoc.services[lowerCaseCategory] = {
+              [lowerCaseService]: {
+                price,
+                sing,
+                type,
+              }
+            }
+            isServiceAdded = true
+          }
+        }
       }
     }
-
+    console.log('entra cuando sale de la creacion de servicio')
     // Solo si se ha añadido el servicio, actualizar la propiedad services de cada usuario
     if (isServiceAdded) {
       console.log('entramos en la edicion de usuarios');
       var users = await User.find();
-      
+
       for (var user of users) {
         if (!user.services) {
           user.services = {};
@@ -81,7 +111,11 @@ const createServicesController = async (service, category, price, sing, type) =>
         }
       }
     }
-
+    if (isServiceAdded) {
+      existingServiceDoc.markModified("services")
+      await existingServiceDoc.save();
+    }
+    console.log('entra si retorna')
     return existingServiceDoc;
   } catch (error) {
     console.error("Error al crear o actualizar el servicio:", error);
--- src/controllers/servicesContollers/deleteServicesController.js
+++ src/controllers/servicesContollers/deleteServicesController.js
@@ -3,49 +3,61 @@ const User = require("../../DB/models/User");
 
 const deleteServicesController = async (service, category) => {
   try {
-    const lowerCaseService = service.toLowerCase();
+    const lowerCaseService = service ? service.toLowerCase() : null;
     const lowerCaseCategory = category.toLowerCase();
 
     // Buscar el servicio en la base de datos
     const existingServiceDoc = await Services.findOne({});
     if (existingServiceDoc) {
-      // Verificar si la categoría y el servicio existen
-      if (
-        existingServiceDoc.services[lowerCaseCategory] &&
-        existingServiceDoc.services[lowerCaseCategory][lowerCaseService]
-      ) {
-        // Eliminar el servicio de la categoría
-        delete existingServiceDoc.services[lowerCaseCategory][lowerCaseService];
-
-        // Si la categoría queda vacía, eliminar también la categoría
-        if (
-          Object.keys(existingServiceDoc.services[lowerCaseCategory]).length ===
-          0
-        ) {
+      // Verificar si la categoría existe
+      if (existingServiceDoc.services[lowerCaseCategory]) {
+        if (lowerCaseService === null) {
+          // Eliminar toda la categoría y sus servicios
+          const servicesToDelete = Object.keys(existingServiceDoc.services[lowerCaseCategory]);
           delete existingServiceDoc.services[lowerCaseCategory];
-        }
 
-        existingServiceDoc.markModified("services");
+          // Eliminar todos los servicios de la categoría en los usuarios
+          const update = {};
+          servicesToDelete.forEach(service => {
+            update[`services.${service}`] = 1;
+          });
 
-        // Eliminar la propiedad del campo services de todos los usuarios
-        await User.updateMany(
-          {},
-          {
-            $unset: {
-              [`services.${lowerCaseCategory}.${lowerCaseService}`]: 1,
-            },
+          await User.updateMany({}, { $unset: update });
+
+          // Marcar la modificación y guardar el documento de servicios
+          existingServiceDoc.markModified("services");
+          await existingServiceDoc.save();
+        } else {
+          // Verificar si el servicio existe en la categoría
+          if (existingServiceDoc.services[lowerCaseCategory][lowerCaseService]) {
+            // Eliminar el servicio de la categoría
+            delete existingServiceDoc.services[lowerCaseCategory][lowerCaseService];
+
+            // Si la categoría queda vacía, eliminar también la categoría
+            if (Object.keys(existingServiceDoc.services[lowerCaseCategory]).length === 0) {
+              delete existingServiceDoc.services[lowerCaseCategory];
+            }
+
+            // Eliminar la propiedad del campo services de todos los usuarios
+            await User.updateMany({}, {
+              $unset: {
+                [`services.${lowerCaseService}`]: 1,
+              }
+            });
+
+            // Marcar la modificación y guardar el documento de servicios
+            existingServiceDoc.markModified("services");
+            await existingServiceDoc.save();
+          } else {
+            // Si el servicio no existe, retornar un mensaje indicando que no existe
+            return { message: "El servicio no existe en la categoría especificada." };
           }
-        );
-
-        // Guardar la actualización en la base de datos
-        await existingServiceDoc.save();
+        }
 
         return existingServiceDoc; // Devolver el servicio actualizado
       } else {
-        // Si la categoría o el servicio no existen, retornar un mensaje indicando que no existe
-        return {
-          message: "El servicio no existe en la categoría especificada.",
-        };
+        // Si la categoría no existe, retornar un mensaje indicando que no existe
+        return { message: "La categoría especificada no existe." };
       }
     } else {
       // Si no existe el documento de servicios, retornar un mensaje indicando que no existe

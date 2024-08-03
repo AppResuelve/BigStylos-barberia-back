498149c7d6ff3d96836d5afa43689c122d147621
tomascallenius <tomascallenius96@gmail.com>
2024-08-03 17:32:50 -0300

    get notification

--- src/controllers/mercadoPagoControllers/createPreferenceController.js
+++ src/controllers/mercadoPagoControllers/createPreferenceController.js
@@ -1,39 +1,40 @@
 //import { MercadoPagoConfig, Preference } from "mercadopago";
-const { MercadoPagoConfig, Preference } = require "mercadopago";
+const { MercadoPagoConfig, Preference } = require("mercadopago");
+const MERCADO_PAGO_ACCESS_TOKEN = process.env.MERCADO_PAGO_ACCESS_TOKEN;
 
-const createPreferenceController = async ( arrayItems ) => {
+const createPreferenceController = async (arrayItems) => {
+  console.log(arrayItems);
+  var items = [];
+  arrayItems.forEach((item) => {
+    items.push({
+      title: item.service.name,
+      quantity: Number(item.quantity),
+      unit_price: Number(item.service.price),
+      currency_id: "ARS",
+    });
+  });
 
-    var items = []
+  const client = new MercadoPagoConfig({
+    accessToken: MERCADO_PAGO_ACCESS_TOKEN,
+  });
 
-    arrayItems.forEach(item => {
-        items.push({
-            title: item.service,
-            quantity: Number(item.quantity),
-            unit_price: Number(price),
-            currency_id: "ARS",
-        })
-    })
-    
-    const client = new MercadoPagoConfig({
-        accessToken: "",
-    })
-  
   try {
     const body = {
-        items,
-        back_urls: {
-            success: "https://www.youtube.com/watch?v=tkOr12AQpnU",
-            failure: "https://www.youtube.com/watch?v=qN4ooNx77u0",
-            pending: "https://www.youtube.com/watch?v=waiu1gimdy8&list=RDwaiu1gimdy8&start_radio=1"
-        },
-        auto_return: "approved",
-    }
+      items,
+      back_urls: {
+        success: "https://www.youtube.com/watch?v=tkOr12AQpnU",
+        failure: "https://www.youtube.com/watch?v=qN4ooNx77u0",
+        pending:
+          "https://www.youtube.com/watch?v=waiu1gimdy8&list=RDwaiu1gimdy8&start_radio=1",
+      },
+      auto_return: "approved",
+      notification_url: "https://appturnos-back.onrender.com/mp_notifications",
+    };
 
-    const preference = new Preference(client)
-    const result = await preference.create({ body })
-    
-    return result.id
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
 
+    return result.id;
   } catch (error) {
     console.error("Error al crear la preferencia:", error);
     throw error;
--- src/routes/routes/mercadoPagoRoutes.js
+++ src/routes/routes/mercadoPagoRoutes.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const createPreferenceHandler = require("../../handlers/mercadoPagoHandlers/createPreferenceHandler.js");
+const getNotificationsHandler = require("../../handlers/mercadoPagoHandlers/getNotificationsHandler.js");
 
 const router = Router();
 
-router.post("/createpreference", createPreferenceHandler);
+router.post("/create_preference", createPreferenceHandler);
+router.post("/mp_notifications", getNotificationsHandler);
+
 
 module.exports = router;

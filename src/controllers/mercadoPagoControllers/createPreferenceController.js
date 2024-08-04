be03862c0df5d941ee6dae94a4249865d2fc34b1
tomascallenius <tomascallenius96@gmail.com>
2024-08-04 13:14:04 -0300

    ajustando

--- src/controllers/mercadoPagoControllers/createPreferenceController.js
+++ src/controllers/mercadoPagoControllers/createPreferenceController.js
@@ -1,16 +1,19 @@
 //import { MercadoPagoConfig, Preference } from "mercadopago";
 const { MercadoPagoConfig, Preference } = require("mercadopago");
 const MERCADO_PAGO_ACCESS_TOKEN = process.env.MERCADO_PAGO_ACCESS_TOKEN;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const createPreferenceController = async (arrayItems) => {
-  console.log(arrayItems);
   var items = [];
   arrayItems.forEach((item) => {
+    let id = `${item.id}+${item.worker[0]}`;
+    console.log(id);
     items.push({
       title: item.service.name,
       quantity: Number(item.quantity),
       unit_price: Number(item.service.price),
       currency_id: "ARS",
+      id,
     });
   });
 
@@ -22,19 +25,19 @@ const createPreferenceController = async (arrayItems) => {
     const body = {
       items,
       back_urls: {
-        success: "https://www.youtube.com/watch?v=tkOr12AQpnU",
-        failure: "https://www.youtube.com/watch?v=qN4ooNx77u0",
+        success: `${FRONTEND_URL}/`,
+        failure: `${FRONTEND_URL}/requestDenied401`,
         pending:
           "https://www.youtube.com/watch?v=waiu1gimdy8&list=RDwaiu1gimdy8&start_radio=1",
       },
       auto_return: "approved",
       notification_url:
-        "https://cd56-181-93-134-26.ngrok-free.app/mercadopago/mp_notifications",
+        "https://ac44-181-93-134-26.ngrok-free.app/mercadopago/mp_notifications",
     };
 
     const preference = new Preference(client);
     const result = await preference.create({ body });
-
+    console.log(result.id, "este es el id de la preferencia");
     return result.id;
   } catch (error) {
     console.error("Error al crear la preferencia:", error);
--- src/handlers/mercadoPagoHandlers/getNotificationsHandler.js
+++ src/handlers/mercadoPagoHandlers/getNotificationsHandler.js
@@ -6,25 +6,33 @@ const client = new MercadoPagoConfig({
 });
 
 const getNotificationsHandler = async (req, res) => {
-  const paymentId = req.query.id;
-  console.log(paymentId);
-  try {
-    const response = await fetch(
-      `https://api.mercadopago.com/v1/payments/${paymentId}`,
-      {
-        method: "GET",
-        headers: {
-          "Authorization": `Bearer ${client.accessToken}`,
-        },
+  console.log(req.query, "este es el req, queryy");
+//   console.log(req.headers, "este es el req, headers");
+  let paymentId;
+  if (req.query.type && req.query.type === "payment") {
+    paymentId = req.query["data.id"];
+    //   console.log(paymentId);
+
+    try {
+      // console.log(paymentId);
+      const response = await fetch(
+        `https://api.mercadopago.com/v1/payments/${paymentId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${client.accessToken}`,
+          },
+        }
+      );
+      if (response) {
+            const data = await response.json();
+            
+        console.log(data.additional_info.items, "este es el data");
       }
-    );
-    if (response) {
-      const data = await response.json();
-      console.log(data);
+      res.sendStatus(200);
+    } catch (error) {
+      res.status(500).json({ message: "Error creating user." });
     }
-    res.sendStatus(200);
-  } catch (error) {
-    res.status(500).json({ message: "Error creating user." });
   }
 };
 

e99fa78661a72d396739905aed5b83a620083bfb
tomascallenius <tomascallenius96@gmail.com>
2024-08-20 11:24:48 -0300

    asd

--- src/handlers/scheduleHandlers/updateNoWorkHandler.js
+++ src/handlers/scheduleHandlers/updateNoWorkHandler.js
@@ -1,9 +1,9 @@
 const updateNoWorkController = require("../../controllers/scheduleControllers/updateNoWorkController");
 
 const updateNoWorkHandler = async (req, res) => {
-  const { noWorkDays } = req.body;
+  const { noWorkDays, daysToCancel } = req.body;
   try {
-    const updatedDays = await updateNoWorkController(noWorkDays);
+    const updatedDays = await updateNoWorkController(noWorkDays, daysToCancel);
     res.status(200).json(updatedDays);
   } catch (error) {
     console.error(error);

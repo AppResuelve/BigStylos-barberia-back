0f38c2a5c22bba9a3a3abc59ef7ca5c1e76c0cc2
F.Fernandez <hellsingsd@gmail.com>
2024-10-08 08:50:56 -0300

    corsOptions OK

--- src/app.js
+++ src/app.js
@@ -10,14 +10,16 @@ server.use(express.json());
 server.use(morgan("dev"));
 
 // Configuración de CORS
- const corsOptions = {
-   origin: "https://tengoturno.up.railway.app", // Reemplaza con tu dominio
-   methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
-   credentials: true, // Si necesitas enviar cookies, tokens, etc.
- };
+const corsOptions = {
+  origin: "https://tengoturno.up.railway.app", // Dominio del frontend
+  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+  credentials: true, // Si necesitas enviar cookies o tokens
+};
 
-server.use(cors());
+// Aplica las opciones de CORS
+server.use(cors(corsOptions));
 
+// Rutas de la API
 server.use("/", routes);
 
 module.exports = server;
